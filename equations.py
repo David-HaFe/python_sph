2701a85e4422036bdab6e84df5dfa5dd67158e81
Mister-Oatbread <david.hafe82@gmail.com>
2026-03-19 12:34:27 +0100

    removed function based approach, and produced less wrong version

--- equations.py
+++ equations.py
@@ -3,55 +3,60 @@
 # equations needed for the sph implementation
 
 # import numpy as np
+import numpy as np
 import jax.numpy as jnp
-from particle import Particle
 
 # implements the discretized navier stokes equation for the ode solver
-def navier_stokes(t, q, particle_a):
-    # extract values from particle a that are needed for all calculations
-    pressure_term = 0
-    viscosity_term = 0
-    x_a = particla_a.x()
-    v_a = particle_a.v()
-    p_a= particle_a.p()
-    rho_a = particle_a.rho()
-    pressure_contribution_a = p_a/jnp.power(rho_a, 2)
-    mu_a = particle_a.mu()
+def navier_stokes(q, t, m, rho, p, mu):
+    h = 1   # -> SPH smoothing length
+    q_dot = np.array([])
 
-    for particle in jnp.nditer(particles):
-        pressure_term += particle.m() * (
-                particle.p()/jnp.power(particle_a.rho(), 2)
-                + pressure_contribution_a
-                ) * delta_W(x_a, particle.x())
+    for a in range(0, no_particles):
+        # reset everything and get new vectors
+        pressure_term = 0
+        viscosity_term = 0
+        x_a = q[2*a]
+        v_a = q[2*a+1]
 
-        delta_x = x_a - particle.x()
-        delta_v = v_a - particle.v()
+        for b in range(0, no_particles):
+            if (W(x_a, x_b) > 0 and not(a == b)):
+                x_b = q[2*b]
+                v_b = q[2*b+1]
 
-        viscosity_term += particle.m() *
-            (mu_a + particle.mu())/(rho_a*particle.rho())
-            *(delta_x*delta_v)/(jnp.power(delta_x, 2) + .01*h**2)
-            * delta_W(x_a, particle.x())
+                pressure_term += m[b] * (
+                    p[b]/jnp.power(rho[b], 2)
+                    + p[a]/jnp.power(rho[a], 2)
+                    ) * delta_W(x_a, x_b)
 
+                delta_x = x_a - x_b
+                delta_v = v_a - v_b
 
-    return rho_a * (pressure_term+viscosity_term)
+                viscosity_term += m[b] * (
+                    (mu[a] + mu[b])/(rho[a]*rho[b])
+                    * (delta_x*delta_v)/(jnp.power(delta_x, 2) + .01*h**2)
+                    ) * delta_W(x_a, x_b)
 
-# kernel for a given point
-# iterate over each particle and collect contributions
-# using Wendland C² kernel
-def W(x_a: jnp.array):
-    kernel_result = 0
+        # fill solution array
+        np.append(q_dot, v_a)
+        np.append(q_dot, rho[a]*(pressure_term + viscosity_term))
 
-    for particle in jnp.nditer(particles):
-        distance = jnp.linalg.norm(x_a - particle.x())/h
+    return q_dot
 
-        if (distance < kernel_radius):
-            kernel_result +=
-            sigma_W/h_dim * (1 + 2*distance)*jnp.power((1 - .5*distance), 4)
+# kernel for a given point and a reference point
+# using C² Wendland kernel
+def W(x_a: jnp.array, x_b: jnp.array):
+    kernel_radius = 2
+    sigma_W = 1
+    h_dim = 1
 
-        return kernel_result
+    distance = jnp.linalg.norm(x_a - x_b)/h
+
+    return jnp.where(distance < kernel_radius,
+        sigma_W/h_dim * (1 + 2*distance)*jnp.power((1 - .5*distance), 4),
+        0.0)
 
 # gradient of the kernel function
 def delta_W(x_a: jnp.array, x_b: jnp.array):
-    return jax.grad(W(x_a), x_b)
+    return jax.grad(W, argnums=1)(x_a, x_b)
 
 
--- main.py
+++ main.py
@@ -4,20 +4,43 @@
 # author: David Hambach Ferrer
 
 # import numpy as np
+import numpy as np
 import jax.numpy as jnp
-import scipy as sp
+from scipy.integrate import odeint
 import matplotlib as plt
-
-from particle import Particle
+from equations import navier_stokes
 
 # constants
 rho_a = 1
-kernel_radius = 2
-h = 1   # -> SPH smoothing length
-particles = np.empty([])
-gravity = np.array([0,-9.81])
+gravity = np.array([0, -9.81])
+no_particles = 4
 
-# initial condition
+# initialize grid
+positions = np.empty([])
+velocities = np.empty([])
 
+for i in range(0, 3):
+    np.append(positions, jnp.array([i, i]))
+    np.append(velocities, jnp.array([-i, -i]))
+
+masses = np.full(no_particles, 1.0)
+densities = np.full(no_particles, 2.0)
+pressures = np.full(no_particles, 1.0)
+dynamic_viscosities = np.full(no_particles, 1.0)
+
+# simulation
+t = np.linspace(0,1,100)
+solution = odeint(
+    lambda q, t: navier_stokes(q,
+                               t,
+                               masses,
+                               densities,
+                               pressures,
+                               dynamic_viscosities),
+    np.array([positions, velocities]),
+    t,
+)
+
+print(solution)
 
 
