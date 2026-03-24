db8064e05b3feb4793d133b2cd5e8d4603fd255e
Jonas Breuling <jonas-breuling@inm.uni-stuttgart.de>
2026-03-24 12:26:40 +0100

    Updated main file and NS rhs.

--- equations.py
+++ equations.py
@@ -13,70 +13,129 @@ gravity = np.array([0, -9.81])
 
 # implements the discretized navier stokes equation for the ode solver
 # q consists of the triple [position,velocity,density] for each particle, all concatenated like this one after the other
-def navier_stokes(t, q, is_wall_particle):
+# def navier_stokes(t, q, is_wall_particle=None):
+def navier_stokes(t, y, is_wall_particle=None):
     diagnostics.time_navier_stokes()
-    no_particles = np.size(q)//5
-    q_dot = np.array([])
+    no_particles = np.size(y)//(2 + 2 + 1)
+    # y_dot = []
+    # x_dot = np.zeros(2 * no_particles)
+    # u_dot = np.zeros(2 * no_particles)
+
+    x = y[:2 * no_particles]
+    u = y[2 * no_particles : 4 * no_particles]
+    rho = y[4 * no_particles :]
+    X = x.reshape(-1, 2)
+    U = u.reshape(-1, 2)
+
+    X_dot = np.zeros((no_particles, 2))
+    U_dot = np.zeros((no_particles, 2))
+    rho_dot = np.zeros(no_particles)
 
     # TODO: these have to be updated, not sure how to do that
     m = np.full(no_particles, 1.0)
     mu = np.full(no_particles, 1.0)
 
-    for a in range(0, no_particles):
-        if not is_wall_particle[a]:
-            # reset everything and get new vectors
-            pressure_term = 0
-            viscosity_term = 0
-            rho_dot = 0
-
-            x_a = jnp.array([q[5*a], q[5*a+1]])
-            v_a = jnp.array([q[5*a+2], q[5*a+3]])
-            rho_a = q[5*a+4]
-            p_a = calculate_pressure(rho_a)
-
-            for b in range(0, no_particles):
-                x_b = jnp.array([q[5*b], q[5*b+1]])
-                particle_close_enough = W(x_a, x_b) > 0
-
-                if not (a == b):
-                    diagnostics.register_particle(particle_close_enough)
-
-                # if (particle_close_enough and not (a == b)):
-                if particle_close_enough:
-
-                    # calculate dv/dt
-                    v_b = jnp.array([q[5*b+2], q[5*b+3]])
-                    rho_b = q[5*b+4]
-                    p_b = calculate_pressure(rho_b)
-
-                    pressure_term += m[b] * (
-                        p_b/jnp.power(rho_b, 2)
-                        + p_a/jnp.power(rho_a, 2)
-                        ) * delta_W(x_a, x_b)
-
-                    delta_x = x_a - x_b
-                    delta_v = v_a - v_b
-
-                    viscosity_term += m[b] * (
-                        (mu[a] + mu[b])/(rho_a*rho_b) * jnp.dot(delta_x, delta_v)
-                        / (jnp.dot(delta_x, delta_x) + .01*h**2)
-                        ) * delta_W(x_a, x_b)
-
-                    # calculate d\rho/dt
-                    rho_dot += m[b]/rho_b*jnp.dot(v_b - v_a, delta_W(x_a, x_b))
-
-            # fill solution array
-            q_dot = np.append(q_dot, v_a)
-            q_dot = np.append(q_dot, - pressure_term + viscosity_term)
-            # q_dot = np.append(q_dot, g - pressure_term + viscosity_term)
-            q_dot = np.append(q_dot, rho_dot)
-
-        else:
-            # is wall particle, just apply dummy solution
-            q_dot = np.append(q_dot, np.array([0, 0, 0, 0, 100]))
-
     diagnostics.time_navier_stokes()
-    return q_dot
+    for i, (Xi, Ui, rhoi) in enumerate(zip(X, U, rho)):
+        pressure_term = jnp.zeros(2)
+        viscosity_term = jnp.zeros(2)
+        rho_doti = 0
+
+        Xi = jnp.array(Xi)
+        pi = calculate_pressure(rhoi)
+        for j, (Xj, Uj, rhoj) in enumerate(zip(X, U, rho)):
+            if i == j:
+                continue
+            Xj = jnp.array(Xj)
+            
+            particle_close_enough = W(Xi, Xj) > 0
+            diagnostics.register_particle(particle_close_enough)
+
+            if particle_close_enough:
+                Uj = jnp.array(Uj)
+                pj = calculate_pressure(rhoj)
+
+                pressure_term += m[j] * (
+                    pj / jnp.power(rhoj, 2) + pi / jnp.power(rhoi, 2)
+                ) * delta_W(Xi, Xj)
+
+                delta_X = Xi - Xj
+                delta_U = Ui - Uj
+
+                viscosity_term += m[j] * (
+                    (mu[i] + mu[j]) / (rhoi * rhoj) * jnp.dot(delta_X, delta_U)
+                    / (jnp.dot(delta_X, delta_X) + .01 * h**2)
+                ) * delta_W(Xi, Xj)
+
+                rho_doti += m[j] / rhoj * jnp.dot(Uj - Ui, delta_W(Xi, Xj))
+
+        X_dot[i] = Ui
+        U_dot[i] = -pressure_term + viscosity_term
+        rho_dot[i] = rho_doti
+    
+    diagnostics.time_navier_stokes()        
+
+    x_dot = X_dot.reshape(-1, order="C")
+    u_dot = U_dot.reshape(-1, order="C")
+    y_dot = np.concatenate((x_dot, u_dot, rho_dot))
+    return y_dot
+
+    # for a in range(no_particles):
+    #     # if not is_wall_particle[a]:
+    #     if True:
+    #         # reset everything and get new vectors
+    #         pressure_term = 0
+    #         viscosity_term = 0
+    #         rho_dot = 0
+
+    #         x_a = jnp.array([q[5*a], q[5*a+1]])
+    #         v_a = jnp.array([q[5*a+2], q[5*a+3]])
+    #         rho_a = q[5*a+4]
+    #         p_a = calculate_pressure(rho_a)
+
+    #         for b in range(0, no_particles):
+    #             x_b = jnp.array([q[5*b], q[5*b+1]])
+    #             particle_close_enough = W(x_a, x_b) > 0
+
+    #             if not (a == b):
+    #                 diagnostics.register_particle(particle_close_enough)
+
+    #             # if (particle_close_enough and not (a == b)):
+    #             if particle_close_enough:
+
+    #                 # calculate dv/dt
+    #                 v_b = jnp.array([q[5*b+2], q[5*b+3]])
+    #                 rho_b = q[5*b+4]
+    #                 p_b = calculate_pressure(rho_b)
+
+    #                 pressure_term += m[b] * (
+    #                     p_b/jnp.power(rho_b, 2)
+    #                     + p_a/jnp.power(rho_a, 2)
+    #                     ) * delta_W(x_a, x_b)
+
+    #                 delta_X = x_a - x_b
+    #                 delta_U = v_a - v_b
+
+    #                 viscosity_term += m[b] * (
+    #                     (mu[a] + mu[b])/(rho_a*rho_b) * jnp.dot(delta_X, delta_U)
+    #                     / (jnp.dot(delta_X, delta_X) + .01*h**2)
+    #                     ) * delta_W(x_a, x_b)
+
+    #                 # calculate d\rho/dt
+    #                 rho_dot += m[b]/rho_b*jnp.dot(v_b - v_a, delta_W(x_a, x_b))
+
+    #         # fill solution array
+    #         y_dot = np.append(y_dot, v_a)
+    #         y_dot = np.append(y_dot, - pressure_term + viscosity_term)
+    #         # q_dot = np.append(q_dot, g - pressure_term + viscosity_term)
+    #         y_dot = np.append(y_dot, rho_dot)
+
+    #     else:
+    #         # is wall particle, just apply dummy solution
+    #         y_dot = np.append(y_dot, np.array([0, 0, 0, 0, 100]))
+
+    # diagnostics.time_navier_stokes()
+    # return y_dot
 
 # calculates the pressure based on given formula
 def calculate_pressure(rho):
--- main.py
+++ main.py
@@ -5,76 +5,123 @@
 
 # import numpy as np
 import numpy as np
+from scipy.integrate import solve_ivp
 
 from integrator import solve
 from equations import navier_stokes
 from diagnostics import diagnostics
 from animation import animate_solution
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 # initialize grid
-initial_condition = np.array([])
-is_wall_particle = np.array([])
+x0 = []
+u0 = []
+rho0 = []
+is_wall_particle = []
+# initial_condition = np.array([])
+# is_wall_particle = np.array([])
 
-x_limit = 2
-y_limit = 2
+x_limit = 4
+y_limit = 4
+n_particle = x_limit * y_limit
 
 for x in range(0, x_limit):
     for y in range(0, y_limit):
         # position
-        initial_condition = np.append(initial_condition, np.array([x, y]))
+        # initial_condition = np.append(initial_condition, np.array([x, y]))
+        x0.extend([x, y])
 
         # velocity
-        initial_condition = np.append(initial_condition, np.array([0, 0]))
+        # initial_condition = np.append(initial_condition, np.array([0, 0]))
+        u0.extend([0, 0])
 
         # initial density
-        initial_condition = np.append(initial_condition, 1)
+        # initial_condition = np.append(initial_condition, 1)
+        rho0.append(1)
 
-        is_wall_particle = np.append(is_wall_particle, False)
+        # is_wall_particle = np.append(is_wall_particle, False)
+        is_wall_particle.append(False)
+
+x0 = np.array(x0, dtype=float)
+u0 = np.array(u0, dtype=float)
+rho0 = np.array(rho0, dtype=float)
+y0 = np.concatenate((x0, u0, rho0))
+is_wall_particle = np.array(is_wall_particle)
+
+# visualize initial configuration
+X0 = x0.reshape(-1, 2)
+fig, ax = plt.subplots()
+ax.set_xlim(-5, 5)
+ax.set_ylim(-5, 5)
+scat = ax.scatter(X0[:, 0], X0[:, 1])
+# plt.show()
 
 dummy_density = 100
-# initialize wall particles
-for x in range(-1, x_limit + 1):
-    # left wall
-    initial_condition = np.append(initial_condition, np.array([x, -1]))
-    # dummy values for velocity and density
-    initial_condition = np.append(
-        initial_condition, np.array([0, 0, dummy_density]))
-    is_wall_particle = np.append(is_wall_particle, True)
+# # initialize wall particles
+# for x in range(-1, x_limit + 1):
+#     # left wall
+#     initial_condition = np.append(initial_condition, np.array([x, -1]))
+#     # dummy values for velocity and density
+#     initial_condition = np.append(
+#         initial_condition, np.array([0, 0, dummy_density]))
+#     is_wall_particle = np.append(is_wall_particle, True)
 
-    # right wall
-    initial_condition = np.append(initial_condition, np.array([x, y_limit+1]))
-    # dummy values for velocity and density
-    initial_condition = np.append(
-        initial_condition, np.array([0, 0, dummy_density]))
-    is_wall_particle = np.append(is_wall_particle, True)
+#     # right wall
+#     initial_condition = np.append(initial_condition, np.array([x, y_limit+1]))
+#     # dummy values for velocity and density
+#     initial_condition = np.append(
+#         initial_condition, np.array([0, 0, dummy_density]))
+#     is_wall_particle = np.append(is_wall_particle, True)
 
-for y in range(-1, y_limit+1):
-    # bottom wall
-    initial_condition = np.append(initial_condition, np.array([-1, y]))
-    # dummy values for velocity and density
-    initial_condition = np.append(
-        initial_condition, np.array([0, 0, dummy_density]))
-    is_wall_particle = np.append(is_wall_particle, True)
+# for y in range(-1, y_limit+1):
+#     # bottom wall
+#     initial_condition = np.append(initial_condition, np.array([-1, y]))
+#     # dummy values for velocity and density
+#     initial_condition = np.append(
+#         initial_condition, np.array([0, 0, dummy_density]))
+#     is_wall_particle = np.append(is_wall_particle, True)
 
 # simulation
 diagnostics.time_ode()
-sol = solve(
-    function=lambda t, q: navier_stokes(t, q, is_wall_particle),
-    initial_condition=initial_condition,
-    dt=.01,
-)
-# sol = solve_ivp(
-#     fun = navier_stokes,
-#     t_span = [0, 1],
-#     y0 = initial_condition,
-#     method = 'RK45',
-#     rtol = 1e-3,
-#     atol = 1e-5,
-#     dense_output = False,
+# sol = solve(
+#     # function=lambda t, q: navier_stokes(t, q, is_wall_particle),
+#     navier_stokes,
+#     # initial_condition=initial_condition,
+#     initial_condition=y0,
+#     dt=.01,
 # )
+
+t0 = 0.0
+t1 = 0.5
+t_span = (t0, t1)
+num = 100
+t_eval = np.linspace(t0, t1, num=num)
+sol = solve_ivp(
+    fun=navier_stokes,
+    t_span=t_span,
+    y0=y0,
+    method = "RK23",
+    # method = "RK45",
+    # method = "BDF",
+    rtol = 1e-1,
+    atol = 1e-1,
+    t_eval=t_eval,
+)
 diagnostics.time_ode()
 
 diagnostics.print_diagnostics()
-animate_solution(sol.t, sol.y)
+# animate_solution(sol.t, sol.y)
 
+def animate(i):
+    yi = sol.y[:, i]
+    xi = yi[:2 * n_particle]
+    Xi = xi.reshape(-1, 2)
+    # scat.set_offsets((Xi[:, 0] - X0[:, 0], Xi[:, 1] - X0[:, 1]))
+    # scat.set_offsets((Xi[:, 0], Xi[:, 1]))
+    scat.set_offsets(Xi)
+    return (scat,)
 
+ani = animation.FuncAnimation(fig, animate, repeat=True, frames=len(sol.t), interval=5)
+
+plt.show()
